7457ff7dc369988d541c1270ee8857278f6788af
ujjawal <ujjawalvats09@gmail.com>
2024-04-23 22:55:09 +0530

    added basic layouts auth and auth tests

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+    return(
+        <div className="bg-blue-800 pb-16">
+            <div className="container mx-auto flex flex-col gap-2">
+                <h1 className="text-5xl text-white font-bold">
+                    Find your next travel destination!
+                </h1>
+                <p className="text-2xl text-white">
+                    Search from the cheapest travel options available online...
+                </p>
+            
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
